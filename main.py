983293e0ca0b195d622417169d7d1c1df4e142fd
SilentSniperX <cocoloverick@gmail.com>
2025-07-08 21:56:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.requests import Request
-
-from news_handler import router as news_router
+from fastapi.staticfiles import StaticFiles
+from news_handler import fetch_finnhub_news
 
 app = FastAPI()
 
-# Static + Templates setup
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Mount static directory (this line assumes there's a 'static' folder in root)
+app.mount("static", StaticFiles(directory="static"), name="static")
+
+# Use the templates folder (direct in root)
 templates = Jinja2Templates(directory="templates")
 
-# Routes
-app.include_router(news_router)
+@app.get("/news")
+async def get_news(request: Request):
+    news = await fetch_finnhub_news()
+    return templates.TemplateResponse("news.html", {"request": request, "news": news})
