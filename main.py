7dc7ae968c677a0ba687d7085a80fbcffc974399
SilentSniperX <cocoloverick@gmail.com>
2025-07-08 20:39:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI
-from news_handler import fetch_and_parse_news
+from news_handler import router as news_router
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Aeryn News API"}
-
-@app.get("/news")
-def get_news():
-    news = fetch_and_parse_news()
-    return news
+app.include_router(news_router)
