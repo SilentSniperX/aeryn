96e1be0ef98be7f3dcd08b0db81fe681ffa45844
SilentSniperX <cocoloverick@gmail.com>
2025-07-08 18:03:53 -0700

    Update news_handler.py

--- news_handler.py
+++ news_handler.py
@@ -1,17 +1,14 @@
 import os
 import requests
-import time
 
 class NewsFetcher:
     def __init__(self):
         self.marketaux_api_key = os.environ.get("MARKETAUX_API_KEY")
 
     def get_marketaux_news(self):
-        # Subtract 2 full days to ensure we're well within the past
-        timestamp = int(time.time()) - 2 * 86400
         url = (
             f"https://api.marketaux.com/v1/news/all?"
-            f"api_token={self.marketaux_api_key}&language=en&limit=10&published_after={timestamp}"
+            f"api_token={self.marketaux_api_key}&language=en&limit=10"
         )
         response = requests.get(url)
         response.raise_for_status()
