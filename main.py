5bd97ca85f7799e7be6d7ca6b099c3968df51d8a
SilentSniperX <cocoloverick@gmail.com>
2025-07-08 17:41:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from news_handler import fetch_and_parse_news
+from news_handler import NewsFetcher
+
 app = FastAPI()
+news_fetcher = NewsFetcher()
 
 @app.get("/")
 def read_root():
-    return {"message": "Aeryn is live, watching the charts..."}
+    return {"message": "Aeryn is live, watching the markets."}
+
 @app.get("/news")
-async def news():
-    data = fetch_and_parse_news()
-    return {"news": sorted(data, key=lambda x: x["score"], reverse=True)}
+async def get_news():
+    data = news_fetcher.fetch_and_parse_news()
+    return data
